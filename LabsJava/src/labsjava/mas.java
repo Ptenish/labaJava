c3304be2cbf64ca7df764c9d2c9e1740cb67de7f
vp-8-508-4 <vp-8-508-4@vp-8-508-4.KLASS.local>
2014-10-23 17:11:15 +0400

    задание 1

---
+++ LabsJava/src/labsjava/mas.java
@@ -0,0 +1,22 @@
+package labsjava;
+
+
+public class mas {
+    public static void main(String[] args) (
+            int n = 10;
+            int[] mas = new int[10];
+            { 
+                for (int i=0; i<10; i++)
+                 mas[i] = (int) (Math.random() * 10);
+                    }
+    
+}
+public static int min(int []A)(
+if(A.length == 0) return 0;
+int min= 0
+    for (int i=1, i<A.length; i++)(
+if (A[i]<A[min] min = i)
+} 
+return A[min];
+}
+}
